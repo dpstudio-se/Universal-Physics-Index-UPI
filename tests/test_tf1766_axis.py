7749acc2977a8960d398052453cbbe7d28235c56
VR-ASI-DP <andreas@wadenholt.se>
2026-09-16 18:18:08 +0200

    Test TF1766 axis mirror invariants

---
+++ tests/test_tf1766_axis.py
@@ -0,0 +1,41 @@
+from upi.tf1766_axis import DEFAULT_AXIS, MirrorOperator, TFAxis, mirror_involution_holds
+
+
+def test_default_axis_is_ordered_and_current_is_unique() -> None:
+    axis = TFAxis(DEFAULT_AXIS)
+    assert [node.year for node in axis.nodes] == [1766, 1809, 1949, 1974, 1992, 1994, 2026]
+    assert axis.current().status == "EST_CURRENT"
+    assert axis.current().label == "CURRENT"
+
+
+def test_historical_nodes_do_not_become_current() -> None:
+    axis = TFAxis(DEFAULT_AXIS)
+    assert all(node.status != "EST_CURRENT" for node in axis.nodes[:-1])
+    assert axis.nodes[0].status == "HIST"
+    assert axis.nodes[1].status == "HIST"
+    assert axis.nodes[2].status == "HIST"
+    assert axis.nodes[3].status == "HIST"
+    assert axis.nodes[4].status == "HIST"
+    assert axis.nodes[5].status == "EST_SOURCE"
+
+
+def test_mirror_index_is_involution() -> None:
+    axis = TFAxis(DEFAULT_AXIS)
+    assert [axis.mirror_index(i) for i in range(7)] == [6, 5, 4, 3, 2, 1, 0]
+    for i in range(7):
+        assert axis.mirror_index(axis.mirror_index(i)) == i
+
+
+def test_mirror_operator_squares_to_identity() -> None:
+    axis = TFAxis(DEFAULT_AXIS)
+    mirror = MirrorOperator(axis)
+    vector = ("1766", "1809", "1949", "1974", "1992", "1994", "2026")
+    assert mirror.square(vector) == vector
+    assert mirror_involution_holds(axis)
+
+
+def test_current_law_is_selected_by_status_not_origin_year() -> None:
+    axis = TFAxis(DEFAULT_AXIS)
+    current = axis.current()
+    assert current.year == 2026
+    assert current.source_type == "current_consolidated_law"
